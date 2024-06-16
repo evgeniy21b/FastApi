441f65c5ab6681d6c175b0b784f7b662084e9436
evgeniy21b <berezin.e1@yandex.ru>
2024-06-16 21:49:31 +0300

    Initial commit

--- wsgi.py
+++ wsgi.py
@@ -6,13 +6,11 @@ path = '/home/manticore1111/FastApi'
 if path not in sys.path:
     sys.path.append(path)
 
-# Установка переменной окружения для PythonAnywhere
+# Установите переменную окружения для конфигурации вашего приложения
 os.environ['PYTHONPATH'] = path
 
-# Импорт вашего приложения FastAPI
-from main import app
+# Импортируйте ваше приложение
+from main import app  # Убедитесь, что 'main' — это файл, содержащий ваше FastAPI приложение
 
-# Убедитесь, что приложение импортировано правильно
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# Это указывается как application в веб-интерфейсе PythonAnywhere
+application = app
